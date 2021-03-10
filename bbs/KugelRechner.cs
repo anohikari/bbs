b33eab3bf1f12c73b3a7254d3cebce9ac37a0ba5
anohikari <coreshine@protonmail.com>
2021-03-10 11:02:13 +0100

    kugelrechner

---
+++ bbs/KugelRechner.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bbs
+{
+    class KugelRechner
+    {
+        private double Durchmesser;
+        private Materialien Material;
+        private enum Materialien
+        {
+            Holz = 0,
+            Alu = 1,
+            Glas = 2,
+            Eisen = 3,
+            Blei = 4
+        }
+        Dictionary<Materialien, double> Gewichte =
+            new Dictionary<Materialien, double>()
+            {
+                {Materialien.Holz, 1.4 },
+                {Materialien.Alu, 2.7 },
+                {Materialien.Glas, 3.0 },
+                {Materialien.Eisen, 7.8 },
+                {Materialien.Blei, 11.3 },
+            };
+
+        public void Main()
+        {
+            Input();
+            Console.WriteLine("Mit Rückgabewert: " + Calculate());
+            double result = 0;
+            Calculate(ref result);
+            Console.WriteLine("Mit Referenz: " + result);
+            Output();
+        }
+
+        private void Input()
+        {
+            Console.WriteLine("Durchmesser ?:");
+            var DurchmesserInput = Console.ReadLine();
+            Console.WriteLine("Type ?:");
+            var MaterialInput = Console.ReadLine();
+            try
+            {
+                Durchmesser = Double.Parse(DurchmesserInput);
+                Material = int.Parse(MaterialInput) switch
+                {
+                    0 => Materialien.Holz,
+                    1 => Materialien.Alu,
+                    2 => Materialien.Glas,
+                    3 => Materialien.Eisen,
+                    4 => Materialien.Blei,
+                };
+            }
+            catch
+            {
+                Console.WriteLine("ya dun goofed");
+            }
+        }
+
+        private double Calculate()
+        {
+            return Gewichte[Material] * (2d / 3d) * Math.PI * Durchmesser;
+        }
+
+        private void Calculate(ref double result)
+        {
+            result = Calculate();
+        }
+
+        private void Output()
+        {
+            Console.WriteLine("Ohne Rückgabewert" + Calculate());
+        }
+
+    }
+}
--- bbs/Program.cs
+++ bbs/Program.cs
@@ -9,8 +9,10 @@ namespace bbs
             //A1.A1Main(new string[0]);
             //A2.A2Main(new string[0]);
             //A3.A3Main(new string[0]);
-            var a6 = new A6();
-            a6.Main();
+            //var a6 = new A6();
+            //a6.Main();
+            var KugelRechner = new KugelRechner();
+            KugelRechner.Main();
         }
     }
 }
